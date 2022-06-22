7d6365a2468169faedaf5baa898a05f68750f426
Hakimi786 <obaidullah.hakimi@tekschool.us>
2022-06-22 09:16:00 -0700

    Personal1

---
+++ src/Gitpersonal/Gitpersonal.java
@@ -0,0 +1,14 @@
+package Gitpersonal;
+
+public class Gitpersonal {
+
+	public static void main(String[] args) {
+		
+		int a = 10;
+		int b = 12;
+		int c = a+b;
+		System.out.println(c);
+
+	}
+
+}
